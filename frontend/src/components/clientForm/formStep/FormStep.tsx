39e84faeb8dbf9d16f00a8fb1c3b311d8ef8772f
Urban Krepel <urban.kreppel@gmail.com>
2023-04-05 15:41:35 +0200

    Update client form

--- frontend/src/components/clientForm/formStep/FormStep.tsx
+++ frontend/src/components/clientForm/formStep/FormStep.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./FormStep.css";
-import { Step } from "../../../pages/clientForm/ClientForm";
+import { Step, StepError } from "../../../pages/clientForm/ClientForm";
 
 const FormStep = ({
   step,
   maxNumberOfInputs,
+  errors
 }: {
   step: Step;
   maxNumberOfInputs: number;
+  errors: StepError[];
 }) => {
   return (
     <div className="clientForm-step">
       {step.inputs.map((input, index) => (
+        <div className="clientForm-step-input">
         <input
           key={index}
           type={input.type}
@@ -19,6 +22,13 @@ const FormStep = ({
           value={input.value}
           onChange={input.onChange}
         />
+
+        {errors.map((error) => {
+          if (error.fieldName === input.fieldName) {
+            return <p className="clientForm-step-error">{error.message}</p>;
+          }
+        })}
+        </div>
       ))}
       {maxNumberOfInputs - step.inputs.length > 0
         ? Array.from(Array(maxNumberOfInputs - step.inputs.length).keys()).map(
--- frontend/src/pages/clientForm/ClientForm.tsx
+++ frontend/src/pages/clientForm/ClientForm.tsx
@@ -3,6 +3,7 @@ import "./ClientForm.css";
 import FormStepDisplay from "../../components/clientForm/formStepDisplay/FormStepDisplay";
 import FormStep from "../../components/clientForm/formStep/FormStep";
 import Buttons from "../../components/clientForm/buttons/Buttons";
+import AllInfromations from "../../components/clientForm/allInformations/AllInfromations";
 
 interface Step {
   displayName: string;
@@ -10,8 +11,28 @@ interface Step {
     type: string;
     placeholder: string;
     value: string;
+    required: boolean;
+    fieldName:
+      | "firstName"
+      | "lastName"
+      | "email"
+      | "phone"
+      | "address"
+      | "city"
+      | "zip"
+      | "country"
+      | "dateOfBirth"
+      | "id_card_number"
+      | "tax_number"
+      | "bank_account_number"
+      | "vat_number";
     onChange: (e: any) => void;
   }[];
+}
+
+interface StepError {
+  fieldName: string;
+  message: string;
 }
 
 const ClientForm = () => {
@@ -34,6 +55,7 @@ const ClientForm = () => {
   const [step, setStep] = React.useState(1);
   const [maxInputs, setMaxInputs] = React.useState(0);
   const [displayOverview, setDisplayOverview] = React.useState(false);
+  const [stepErrors, setStepErrors] = React.useState<StepError[]>([]);
 
   const handleChange = (input: any) => (e: any) => {
     setData({ ...data, [input]: e.target.value });
@@ -47,18 +69,24 @@ const ClientForm = () => {
           type: "text",
           placeholder: "First Name",
           value: data.firstName,
+          required: true,
+          fieldName: "firstName",
           onChange: handleChange("firstName"),
         },
         {
           type: "text",
           placeholder: "Last Name",
           value: data.lastName,
+          required: true,
+          fieldName: "lastName",
           onChange: handleChange("lastName"),
         },
         {
           type: "date",
           placeholder: "Date of Birth",
           value: data.dateOfBirth,
+          required: true,
+          fieldName: "dateOfBirth",
           onChange: handleChange("dateOfBirth"),
         },
       ],
@@ -67,15 +95,19 @@ const ClientForm = () => {
       displayName: "Contact Information",
       inputs: [
         {
-          type: "text",
+          type: "email",
           placeholder: "Email",
           value: data.email,
+          required: true,
+          fieldName: "email",
           onChange: handleChange("email"),
         },
         {
           type: "text",
           placeholder: "Phone number",
           value: data.phone,
+          required: true,
+          fieldName: "phone",
           onChange: handleChange("phone"),
         },
       ],
@@ -87,24 +119,32 @@ const ClientForm = () => {
           type: "text",
           placeholder: "Address",
           value: data.address,
+          required: true,
+          fieldName: "address",
           onChange: handleChange("address"),
         },
         {
           type: "text",
           placeholder: "City",
           value: data.city,
+          required: true,
+          fieldName: "city",
           onChange: handleChange("city"),
         },
         {
           type: "text",
           placeholder: "Zip",
           value: data.zip,
+          required: true,
+          fieldName: "zip",
           onChange: handleChange("zip"),
         },
         {
           type: "text",
           placeholder: "Country",
           value: data.country,
+          required: true,
+          fieldName: "country",
           onChange: handleChange("country"),
         },
       ],
@@ -116,39 +156,75 @@ const ClientForm = () => {
           type: "text",
           placeholder: "ID Card Number",
           value: data.id_card_number,
+          required: true,
+          fieldName: "id_card_number",
           onChange: handleChange("id_card_number"),
         },
         {
           type: "text",
           placeholder: "Tax Number",
           value: data.tax_number,
+          required: true,
+          fieldName: "tax_number",
           onChange: handleChange("tax_number"),
         },
         {
           type: "text",
           placeholder: "Bank Account Number",
           value: data.bank_account_number,
+          required: true,
+          fieldName: "bank_account_number",
           onChange: handleChange("bank_account_number"),
         },
         {
           type: "text",
           placeholder: "VAT Number",
           value: data.vat_number,
+          required: true,
+          fieldName: "vat_number",
           onChange: handleChange("vat_number"),
         },
       ],
     },
   ];
 
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const validateStep = (): boolean => {
+    const currentErros: StepError[] = [];
+    const currentStep = steps[step - 1];
+    currentStep.inputs.forEach((input) => {
+      const dataValue = data[input.fieldName];
+      if (input.required && dataValue === "") {
+        currentErros.push({
+          fieldName: input.fieldName,
+          message: "This field is required",
+        });
+      } else if (input.type === "email" && !validateEmail(dataValue)) {
+        currentErros.push({
+          fieldName: input.fieldName,
+          message: "Invalid email",
+        });
+      }
+    });
+    setStepErrors(currentErros);
+    return currentErros.length === 0;
+  };
+
   const nextStep = () => {
-    if (step < steps.length) setStep(step + 1);
+    if (step < steps.length && validateStep()) setStep(step + 1);
   };
 
   const prevStep = () => {
     if (step > 1) setStep(step - 1);
   };
 
-  const handleSubmit = () => {}
+  const showOverview = () => {
+    if (validateStep()) setDisplayOverview(true);
+  };
 
   useEffect(() => {
     let max = 0;
@@ -161,19 +237,34 @@ const ClientForm = () => {
   return (
     <div className="clientForm">
       <div className="clientFormContainer">
-        <p className="clientFormTitle">{steps[step - 1].displayName}</p>
-        <FormStepDisplay currentStep={step} numberOfSteps={steps.length} />
-        <FormStep step={steps[step - 1]} maxNumberOfInputs={maxInputs} />
-        <Buttons
-          handleNext={step < steps.length ? nextStep : handleSubmit}
-          handleBack={prevStep}
-          displayBack={step > 1}
-          nextDisplayName={step < steps.length ? "Next" : "Submit" }
-        />
+        <p className="clientFormTitle">
+          {!displayOverview
+            ? steps[step - 1].displayName
+            : "Check your information"}
+        </p>
+        {!displayOverview && (
+          <FormStepDisplay currentStep={step} numberOfSteps={steps.length} />
+        )}
+        {!displayOverview && (
+          <FormStep
+            step={steps[step - 1]}
+            maxNumberOfInputs={maxInputs}
+            errors={stepErrors}
+          />
+        )}
+        {!displayOverview && (
+          <Buttons
+            handleNext={step < steps.length ? nextStep : showOverview}
+            handleBack={prevStep}
+            displayBack={step > 1}
+            nextDisplayName={step < steps.length ? "Next" : "Submit"}
+          />
+        )}
+        {displayOverview && <AllInfromations data={data} steps={steps} />}
       </div>
     </div>
   );
 };
 
 export default ClientForm;
-export type { Step };
+export type { Step, StepError };
